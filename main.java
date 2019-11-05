d761ac5445bd52192a43789a8afa88b5bd097c81
unknown <iman.widyatama@pegadaian.co.id>
2019-11-05 10:50:18 +0700

    Update main.java

--- main.java
+++ main.java
@@ -5,7 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Version1");
-		System.out.println("Hello World1");		
+		System.out.println("Hello World1");	
+		System.out.println("BIGREDS");	
 
 	}
 
