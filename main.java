8a6d3c6634acf3c5a35a9809923b58e64063c8bf
unknown <iman.widyatama@pegadaian.co.id>
2019-11-05 09:57:44 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+
+import java.io.*;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Version1");
+		System.out.println("Hello World1");		
+
+	}
+
+}
